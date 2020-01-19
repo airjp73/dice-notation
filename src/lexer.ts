9eb671d6e0e14696f54a1c1ef26459319aa944fc
airjp73 <aaron.pettengill.violin@gmail.com>
2020-01-19 15:05:40 -0500

    Scaffold lexer

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,5 +1,46 @@
-import tokenize from './tokenize';
-import { DiceNotationNode } from './nodes';
-import { Token } from './tokens';
+import { DiceNotationNode, NodeType } from './nodes';
+import {
+  Token,
+  ConstantToken,
+  DiceRollToken,
+  OperatorToken,
+  TokenType,
+  OpenParenToken,
+  CloseParenToken,
+} from './tokens';
 
-export default function lex(tokens: Token[]): DiceNotationNode {}
+export default function lex(tokens: Token[]): DiceNotationNode | null {
+  let head: DiceNotationNode | null = null;
+
+  function handleConstant(token: ConstantToken) {}
+
+  function handleDiceRoll(token: DiceRollToken) {}
+
+  function handleOperator(token: OperatorToken) {}
+
+  function handleOpenParen(token: OpenParenToken) {}
+
+  function handleCloseParen(token: CloseParenToken) {}
+
+  tokens.forEach(token => {
+    switch (token.type) {
+      case TokenType.Constant:
+        handleConstant(token);
+        break;
+      case TokenType.DiceRoll:
+        handleDiceRoll(token);
+        break;
+      case TokenType.Operator:
+        handleOperator(token);
+        break;
+      case TokenType.OpenParen:
+        handleOpenParen(token);
+        break;
+      case TokenType.CloseParen:
+        handleCloseParen(token);
+        break;
+    }
+  });
+
+  return head;
+}
