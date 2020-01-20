996095226537ce43dd1c2aab32cc4e4488e02dc8
airjp73 <aaron.pettengill.violin@gmail.com>
2020-01-20 12:08:41 -0500

    Delete lexer stuff

--- src/LexingError.ts
+++
@@ -1,9 +0,0 @@
-import { Token } from './tokens';
-
-class LexingError extends Error {
-  constructor(message: string, token: Token) {
-    super(`${message}: ${token.content} at position ${token.position}`);
-  }
-}
-
-export default LexingError;
