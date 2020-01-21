9e3736c098c1c77ff7bf5e243d329f0719402cce
airjp73 <aaron.pettengill.violin@gmail.com>
2020-01-20 20:04:20 -0500

    Add some more test cases

--- src/__test__/processTokens.test.ts
+++ src/__test__/processTokens.test.ts
@@ -13,6 +13,11 @@ describe('processTokens', () => {
 
     // Constants to truly verify operator precedence
     ['2 + 3 * (4 + 2)', 20, 20, 1],
+    ['4*4 + 4 / 2 -10', 8, 8, 1],
+    ['(2 + 2) * (3 * (3 - 1)) - (3 + 1)', 20, 20, 1],
+
+    // TODO: Support negatives?
+    ['64 * (0-3) - 4008 / 5 + (0-328)', -1321.6, -1321.6, 1],
   ];
 
   it.each(cases)('should correctly roll %s', (notation, min, max, repeats) => {
