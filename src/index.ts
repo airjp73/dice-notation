b3698e963a0d45a24bce539954fc8d3cea8e771a
airjp73 <aaron.pettengill.violin@gmail.com>
2020-01-24 22:57:35 -0500

    Export `random`

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import createDiceRoller from './createDiceRoller';
 
 export { default as createDiceRoller } from './createDiceRoller';
 export { default as withPlugins } from './withPlugins';
+export { default as random } from './util/random';
 
 export const {
   tokenize,
